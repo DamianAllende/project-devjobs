c9f63c013d068979ce82f903a4a5b5e7023442f1
DamianAllende <amigosaburridos@gmail.com>
2018-04-15 19:35:18 -0500

    committing work from part-02 HomeWork

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-const fs = require('fs')
+//const fs = require('fs')
 const express = require('express')
+const ejs = require('ejs')
+
+
+const pageRouter = require('./src/routers/pageRouter')
+const apiRouter = require('./src/routers/apiRouter')
+
+
 const app = express()
 
+app.engine('ejs', ejs.renderFile)
+app.set('view engine', 'ejs')
+app.set('views', `${__dirname}/src/views`)
+
+app.use('/', pageRouter)
+app.use('/about', pageRouter)
+
+app.use('/api', apiRouter)
 
 
-app.get('/', function(req, res){
-	fs.readFile(`${__dirname}/src/views/home.html`, 'utf-8', function(error, content){
-		res.send(content)
-	})
+// app.get('/', function(req, res){
+// 	fs.readFile(`${__dirname}/src/views/home.html`, 'utf-8', function(error, content){
+// 		res.send(content)
+// 	})
 	
-})
+// })
 
 
 process.env.PORT = 3000
